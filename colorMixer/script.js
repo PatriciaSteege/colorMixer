998b60b5281031f281341da8248d780dd708b67f
Patricia Steege <steegepatricia@gmail.com>
2023-09-20 10:48:44 +0200

    feat: colorMixer ready

--- colorMixer/script.js
+++ colorMixer/script.js
@@ -1,24 +1,38 @@
 const wrapper = document.querySelector("#input-wrapper");
 const btn = document.querySelector("button");
 const url = "https://dummy-apis.netlify.app/api/color";
-let redInput = document.querySelector("#red");
-let greenInput = document.querySelector("#green");
-let blueInput = document.querySelector("#blue");
+const redInput = document.querySelector("#red");
+const greenInput = document.querySelector("#green");
+const blueInput = document.querySelector("#blue");
 
 wrapper.addEventListener("input", changeColor);
 
-const changeColor = () => {
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  randomizeColor();
+});
+
+function changeColor() {
+  let red = redInput.value;
+  let green = greenInput.value;
+  let blue = blueInput.value;
+
+  document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+}
+changeColor();
+
+function randomizeColor() {
   fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((color) => {
       console.log(color);
-      redInput = color.rgb.r;
-      greenInput = color.rgb.g;
-      blueInput = color.rgb.b;
-    });
-};
-changeColor();
+      redInput.value = color.rgb.r;
+      greenInput.value = color.rgb.g;
+      blueInput.value = color.rgb.b;
 
-btn.addEventListener("click", changeColor);
+      changeColor();
+    });
+}
+randomizeColor();
